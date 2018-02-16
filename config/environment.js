de7634a09129e65817edd066cc3bc45c665a7a25
rake7h <rake7h@gmail.com>
2018-02-17 03:36:09 +0530

    rootURL (#7)

--- config/environment.js
+++ config/environment.js
@@ -45,7 +45,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/light/';
+    ENV.rootURL = '/maya-light/';
     // here you can enable a production-specific feature
   }
 
