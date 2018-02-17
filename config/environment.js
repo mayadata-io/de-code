a5295ab6f85aa5b71c74b78a1e7ad2b57b37942c
rake7h <rake7h@gmail.com>
2018-02-17 17:23:52 +0530

    url changed (#17)

--- config/environment.js
+++ config/environment.js
@@ -45,7 +45,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.baseURL = '/maya-light';
+    ENV.rootURL = '/maya-light';
     ENV.locationType = 'hash';
   }
 
