994c32094ecfbe04b66c79c289aeedf60a102284
rake7h <rake7h@gmail.com>
2018-02-17 17:16:30 +0530

    dist with app route

--- config/environment.js
+++ config/environment.js
@@ -45,6 +45,8 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.baseURL = '/maya-light';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
