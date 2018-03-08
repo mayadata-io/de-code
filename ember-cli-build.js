9e0d35866fb05e6439b98c15318f036d057e0713
rake7h <rake7h@gmail.com>
2018-03-08 19:05:58 +0530

    icons initial check in (#26)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -24,6 +24,12 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  //icons
+  app.import('vendor/icons/mayaicon/style.css');
+  app.import('vendor/icons/mayaicon/fonts/mayaicon.eot',   { destDir: 'assets/fonts/'});
+  app.import('vendor/icons/mayaicon/fonts/mayaicon.svg',   { destDir: 'assets/fonts/'});
+  app.import('vendor/icons/mayaicon/fonts/mayaicon.ttf',   { destDir: 'assets/fonts/'});
+  app.import('vendor/icons/mayaicon/fonts/mayaicon.woff',  { destDir: 'assets/fonts/'});
 
   return app.toTree();
 };
