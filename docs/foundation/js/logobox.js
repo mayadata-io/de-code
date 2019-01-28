99946e28561b2eeaf3dcf1a999eb333b7a4c72d2
visualapps <rakesh.singh@mayadata.io>
2019-01-28 22:44:19 +0530

    Update logobox.js

--- docs/foundation/js/logobox.js
+++ docs/foundation/js/logobox.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
+const logoTheme ='hello'
+
+function changeTheme(theme) {
+  let currentTheme = theme;
+  if(currentTheme === light) {
+    logoTheme = dark
+  }
+  else {
+    logoTheme = light
+  }
+}
 const LogoBox = (props) => (
   <div class="logobox">
     <div class="logobox-header">
       <div class="ml-auto">
-        <a href={props.light} target="_blank"><span class="fa fa-download"></span></a>
+        <span class="mx-2">
+         <a ><span class="fa fa-download"></span></a>
+        </span>
+        <span class="mx-2">
+         <span><span class="fa fa-link"></span></span>
+        </span>
+        <span class="mx-2">
+         <span onClick={changeTheme }><span class="fa fa-adjust"></span></span>
+        </span>
       </div>
     </div>
     <div class="logobox-content">
