8b0b4b0b5e16e2d5477a26051a09ed1b2aceaf34
visualapps <rakesh.singh@mayadata.io>
2019-01-30 23:58:51 +0530

    Added remaining colors and create primary and secondary catg.

--- docs/products/components/colorblock.js
+++ docs/products/components/colorblock.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+function yiq(hex) {
+      var r = parseInt(hex.substr(1, 2), 16),
+          g = parseInt(hex.substr(3, 2), 16),
+          b = parseInt(hex.substr(5, 2), 16),
+          yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+      return (yiq >= 128) ? 'text-dark' : 'text-light';
+}
 function ColorBlock(props) {
     return (
       <div className="color-block-wrapper">
         { props.name.map((color) =>
           <div className={`color-variant-block bg-${ color.name }`} key={ color.name }>
-            <div class="color-details">
-              <div className="">{color.name}</div>
+            <div className="color-details">
+              <div className={yiq(color.hax)}><span className="font-weight-normal">{color.name}</span></div>
             </div>
           </div>
         )
