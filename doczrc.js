5979d77ac8e898b5ae8a08576d33971e985cb423
visualapps <rakesh.singh@mayadata.io>
2019-02-05 17:51:31 +0530

    added table components

--- doczrc.js
+++ doczrc.js
@@ -20,12 +20,27 @@ export default {
   themeConfig: {
     mode: 'light',
     colors: {
+      white: '#FFFFFF',
+      grayExtraLight: '#EEF1F5',
+      grayLight: '#CED4DE',
+      gray: '#7D899C',
+      grayDark: '#383842',
+      grayExtraDark: '#0F0F11',
+      dark: '#23232A',
+      blue: '#0063FF',
+
+      /** properties bellow depends on mode select */
+      primary: '#0063FF',
+      text: '#383842',
+      link: '#0063FF',
       sidebarBg: 'white',
+      footerText: '#383842',
+      sidebarText: '#23232A',
     },
     styles: {
       body: {
-        fontFamily:
-          '-apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif',
+        fontFamily: '-apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif',
+        fintSize: 14, //default
       },
       h1: {
         fontSize: '4em',
