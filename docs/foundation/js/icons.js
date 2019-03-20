0f88b21b430d2475314ec0b1a6a48db3c6fca2f7
visualapps <rakesh.singh@mayadata.io>
2019-03-20 15:47:13 +0530

    Added search bar in icons

--- docs/foundation/js/icons.js
+++ docs/foundation/js/icons.js
@@ -4,23 +4,39 @@ class Icons extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      icons: 'icons'
-    }
+      searchIcons: IconLists ,
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({input: event.target.value})
+
+    let allIcons = JSON.stringify(IconLists);
+    let input = this.state.input;
+    var inputRegex = new RegExp(input, 'i');
+
+    let searchResult = IconLists.filter(IconLists, function(IconLists) {
+      return allIcons.match(inputRegex);
+      console.log('searchResult',allIcons.match(inputRegex));
+    })
   }
   render() {
     return(
       <div>
         <div>
-          <h3>{IconLists.length} Icons</h3>
-        </div>
-      <div className="d-flex flex-wrap">
-          { IconLists.map((icon) =>
-          <div className="icon-box-content">
-            <span className={`mi mi-1x mr-3 ${icon.name}`} key={icon.name}></span>
-            <span>{icon.label}</span>
+          <div className="mt-5 mb-4 ">
+            <input className="form-control form-control-lg" placeholder={`Search ${IconLists.length} icons`} value={this.state.value} onChange={this.handleChange} />
           </div>
-          )
-         }
+        </div>
+        <div className="d-flex flex-wrap">
+          { this.state.searchIcons.map((icon) =>
+            <div className="icon-box-content" key={icon.name}>
+              <span key={icon.name}className={`mi mi-1x mr-3 ${icon.name}`} key={icon.name}></span>
+              <span key={icon.label}>{icon.label}</span>
+            </div>
+            )
+          }
          </div>
       </div>
     );
