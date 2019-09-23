0921185f433d7caac3b83b02543d7036f94f02bd
Amrish Kushwaha <askmaurya48@gmail.com>
2019-09-23 21:33:34 +0530

    Add logos
    Signed-off-by: Amrish Kushwaha <askmaurya48@gmail.com>
    
    This commit will add
    - director-online logos
    - director-onprem logos
    - openebs enterprise logos
    and will update
    - openebs logos
    - litmus logos
    - mayadata logos

--- docs/foundation/js/logobox.js
+++ docs/foundation/js/logobox.js
@@ -1,45 +1,54 @@
-import React from 'react';
+import React from "react";
 
 class LogoBox extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      theme:     'logo-light', //default
-      logo:       props.light, //default
-      download:   props.download,
-      logoLight:  props.light,
-      logoDark:   props.dark,
-    }
+      theme: "logo-light", //default
+      logo: props.light, //default
+      download: props.download,
+      logoLight: props.light,
+      logoDark: props.dark
+    };
   }
   changeTheme() {
     let currentTheme = this.state.theme;
-    console.log('currentTheme', currentTheme, 'logo', this.state.logo)
-    if(currentTheme === 'logo-dark') {
-      this.setState({theme: 'logo-light', logo: this.state.logoLight})
-    }
-    else {
-      this.setState({theme: 'logo-dark', logo: this.state.logoDark})
+    console.log("currentTheme", currentTheme, "logo", this.state.logo);
+    if (currentTheme === "logo-dark") {
+      this.setState({ theme: "logo-light", logo: this.state.logoLight });
+    } else {
+      this.setState({ theme: "logo-dark", logo: this.state.logoDark });
     }
   }
   download() {
-    console.log('download', this.state.download);
+    console.log("download", this.state.download);
     window.open(this.state.download);
   }
   render() {
-    return(
+    return (
       <div className="logobox">
         <div className="logobox-header">
           <div className="ml-auto">
             <span>
-             <button className="btn btn-link" onClick={this.download.bind(this)}><span className="fa fa-download"></span></button>
+              <button
+                className="btn btn-link"
+                onClick={this.download.bind(this)}
+              >
+                <span className="fa fa-download" />
+              </button>
             </span>
             <span>
-             <button className="btn btn-link" onClick={this.changeTheme.bind(this)}><span className="fa fa-adjust"></span></button>
+              <button
+                className="btn btn-link"
+                onClick={this.changeTheme.bind(this)}
+              >
+                <span className="fa fa-adjust" />
+              </button>
             </span>
           </div>
         </div>
         <div className={`logobox-content ${this.state.theme}`}>
-         <img src={this.state.logo} className="img-fluid"/>
+          <img src={this.state.logo} className="img-fluid" />
         </div>
       </div>
     );
--- doczrc.js
+++ doczrc.js
@@ -1,51 +1,51 @@
 // doczrc.js
-import { css } from 'docz-plugin-css'
+import { css } from "docz-plugin-css";
 
 export default {
-  title: ' 🎄  De - code',
-  description: 'Style guide for MayaData products.',
-  indexHtml: 'docs/index.html',
+  title: " 🎄  De - code",
+  description: "Style guide for MayaData products.",
+  indexHtml: "docs/index.html",
   codeSandbox: false,
   hashRouter: true,
-  port: '9999',
-  src: 'docs',
-  dest: 'dist',
-  base: '/de-code/', //gh-page
-
+  port: "9999",
+  src: "docs",
+  dest: "dist",
+  base: "/de-code/", //gh-page
   plugins: [
     css({
-      preprocessor: 'sass'
+      preprocessor: "sass"
     })
   ],
   themeConfig: {
-    mode: 'light',
+    mode: "light",
     colors: {
-      white: '#FFFFFF',
-      grayExtraLight: '#EEF1F5',
-      grayLight: '#CED4DE',
-      gray: '#7D899C',
-      grayDark: '#383842',
-      grayExtraDark: '#0F0F11',
-      dark: '#23232A',
-      blue: '#0063FF',
+      white: "#FFFFFF",
+      grayExtraLight: "#EEF1F5",
+      grayLight: "#CED4DE",
+      gray: "#7D899C",
+      grayDark: "#383842",
+      grayExtraDark: "#0F0F11",
+      dark: "#23232A",
+      blue: "#0063FF",
 
       /** properties bellow depends on mode select */
-      primary: '#0063FF',
-      text: '#383842',
-      link: '#0063FF',
-      sidebarBg: 'white',
-      footerText: '#383842',
-      sidebarText: '#23232A',
+      primary: "#0063FF",
+      text: "#383842",
+      link: "#0063FF",
+      sidebarBg: "white",
+      footerText: "#383842",
+      sidebarText: "#23232A"
     },
     styles: {
       body: {
-        fontFamily: '-apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif',
-        fontSize: 14, //default
+        fontFamily:
+          "-apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif",
+        fontSize: 14 //default
       },
       h1: {
-        fontSize: '4em',
-        fontWeight: 800,
-      },
-    },
-  },
-}
+        fontSize: "4em",
+        fontWeight: 800
+      }
+    }
+  }
+};
