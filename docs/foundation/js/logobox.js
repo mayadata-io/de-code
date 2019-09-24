be71cdd8af12da6c7d600735636904a6ca8197d4
Amrish Kushwaha <askmaurya48@gmail.com>
2019-09-24 18:06:37 +0530

    Add name field for every card and refactor code
    Signed-off-by: Amrish Kushwaha <askmaurya48@gmail.com>

--- docs/foundation/js/logobox.js
+++ docs/foundation/js/logobox.js
@@ -2,46 +2,42 @@ import React from "react";
 
 class LogoBox extends React.Component {
   constructor(props) {
-    super(props);
+    super();
     this.state = {
       theme: "logo-light", //default
-      logo: props.light, //default
-      download: props.download,
-      logoLight: props.light,
-      logoDark: props.dark
+      logo: props.light //default
     };
+    this.download = this.download.bind(this);
+    this.changeTheme = this.changeTheme.bind(this);
   }
   changeTheme() {
     let currentTheme = this.state.theme;
     console.log("currentTheme", currentTheme, "logo", this.state.logo);
     if (currentTheme === "logo-dark") {
-      this.setState({ theme: "logo-light", logo: this.state.logoLight });
+      this.setState({ theme: "logo-light", logo: this.props.light });
     } else {
-      this.setState({ theme: "logo-dark", logo: this.state.logoDark });
+      this.setState({ theme: "logo-dark", logo: this.props.dark });
     }
   }
   download() {
-    console.log("download", this.state.download);
-    window.open(this.state.download);
+    console.log("download", this.props.download);
+    window.open(this.props.download);
   }
   render() {
     return (
       <div className={`logobox h-100 ${this.state.theme}`}>
         <div className="logobox-header">
+          <div className="my-auto mr-auto ml-3">
+            <span className="logobox-header-title">{this.props.name}</span>
+          </div>
           <div className="ml-auto">
             <span>
-              <button
-                className="btn btn-link"
-                onClick={this.download.bind(this)}
-              >
+              <button className="btn btn-link" onClick={this.download}>
                 <span className="fa fa-download" />
               </button>
             </span>
             <span>
-              <button
-                className="btn btn-link"
-                onClick={this.changeTheme.bind(this)}
-              >
+              <button className="btn btn-link" onClick={this.changeTheme}>
                 <span className="fa fa-adjust" />
               </button>
             </span>
