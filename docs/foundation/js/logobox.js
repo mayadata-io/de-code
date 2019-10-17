459e4f73f4b2ba13ebc45aa1138a67ac57c380cf
Amrish Kushwaha <askmaurya48@gmail.com>
2019-10-17 19:24:51 +0530

    Update branding logos (#103)
    * Update branding logos
    
    Signed-off-by: Amrish Kushwaha <askmaurya48@gmail.com>
    
    This commit will
    - add director-online, director-onprem and openebs-enterprise-platform
    logos
    - update openebs, litmus and mayadata logos
    - add public-resources director which will contains all of zip files of
    the logos
    
    * Restructure the logos cards
    
    Signed-off-by: Amrish Kushwaha <askmaurya48@gmail.com>
    
    * Add padding for x direction in logos card
    
    Signed-off-by: Amrish Kushwaha <askmaurya48@gmail.com>
    
    * Add name field for every card and refactor code
    
    Signed-off-by: Amrish Kushwaha <askmaurya48@gmail.com>
    
    * Fix padding for download and toggle icons
    
    Signed-off-by: Amrish Kushwaha <askmaurya48@gmail.com>
    
    This commit will fix
    - padding for download and toggle icons
    
    * Add new branding logos for company and products
    
    Signed-off-by: Amrish Kushwaha <askmaurya48@gmail.com>
    
    This commit will add new logos of
    - mayadata
    - openebs-enterprise-platform
    - director-onprem
    - director-online
    - litmus
    - company icon
    - litmus icon
    
    * Add openebs-cas logo
    
    Signed-off-by: Amrish Kushwaha <askmaurya48@gmail.com>
    
    * Add openebs-director logos and update old logos
    
    Signed-off-by: Amrish Kushwaha <askmaurya48@gmail.com>

--- docs/foundation/js/logobox.js
+++ docs/foundation/js/logobox.js
@@ -1,45 +1,56 @@
-import React from 'react';
+import React from "react";
 
 class LogoBox extends React.Component {
   constructor(props) {
-    super(props);
+    super();
     this.state = {
-      theme:     'logo-light', //default
-      logo:       props.light, //default
-      download:   props.download,
-      logoLight:  props.light,
-      logoDark:   props.dark,
-    }
+      theme: "logo-light", //default
+      logo: props.light //default
+    };
+    this.download = this.download.bind(this);
+    this.changeTheme = this.changeTheme.bind(this);
   }
   changeTheme() {
     let currentTheme = this.state.theme;
-    console.log('currentTheme', currentTheme, 'logo', this.state.logo)
-    if(currentTheme === 'logo-dark') {
-      this.setState({theme: 'logo-light', logo: this.state.logoLight})
-    }
-    else {
-      this.setState({theme: 'logo-dark', logo: this.state.logoDark})
+    console.log("currentTheme", currentTheme, "logo", this.state.logo);
+    if (currentTheme === "logo-dark") {
+      this.setState({ theme: "logo-light", logo: this.props.light });
+    } else {
+      this.setState({ theme: "logo-dark", logo: this.props.dark });
     }
   }
   download() {
-    console.log('download', this.state.download);
-    window.open(this.state.download);
+    console.log("download", this.props.download);
+    window.open(this.props.download);
   }
   render() {
-    return(
-      <div className="logobox">
+    return (
+      <div className={`logobox h-100 ${this.state.theme}`}>
         <div className="logobox-header">
+          <div>
+            <span className="logobox-header-title">{this.props.name}</span>
+          </div>
           <div className="ml-auto">
             <span>
-             <button className="btn btn-link" onClick={this.download.bind(this)}><span className="fa fa-download"></span></button>
+              <button
+                className="btn btn-link pl-2 pr-1"
+                onClick={this.download}
+              >
+                <span className="fa fa-download" />
+              </button>
             </span>
             <span>
-             <button className="btn btn-link" onClick={this.changeTheme.bind(this)}><span className="fa fa-adjust"></span></button>
+              <button
+                className="btn btn-link pl-1 pr-2"
+                onClick={this.changeTheme}
+              >
+                <span className="fa fa-adjust" />
+              </button>
             </span>
           </div>
         </div>
-        <div className={`logobox-content ${this.state.theme}`}>
-         <img src={this.state.logo} className="img-fluid"/>
+        <div className="logobox-content">
+          <img src={this.state.logo} className="img-fluid" />
         </div>
       </div>
     );
